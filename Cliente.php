0f9647b0d9211d51f9d72c1c256db9904eef009d
David Moura <davidmoura@gmail.com>
2016-03-20 17:36:43 -0300

    Adicionado Layout (Bootstrap) na listagem de clientes e botão para ordenação

--- Cliente.php
+++ Cliente.php
@@ -20,13 +20,13 @@ class Cliente
 
 $cliente = array();
 
-$cliente[0] = new Cliente("1","David","96031536","01019200910","david@teste.com");
-$cliente[1] = new Cliente("2","Vicente","96565456","12548512547","vicente@teste.com");
-$cliente[2] = new Cliente("3","Patrick","92547411","00015215222","patrick@teste.com");
-$cliente[3] = new Cliente("4","Sirlei","91221122","11221122122","sirlei@teste.com");
-$cliente[4] = new Cliente("5","Sato","94784512","52885522112","sirlei@teste.com");
-$cliente[5] = new Cliente("6","Wesley","91227788","99556622321","wesley@safadao.com");
-$cliente[6] = new Cliente("7","Natacha","95774411","99552211447","natacha@teste.com");
-$cliente[7] = new Cliente("8","Rogerio","99992211","11448855221","rogerio@teste.com");
-$cliente[8] = new Cliente("9","Guilherme","91242424","24242524242","guilherme@teste.com");
-$cliente[9] = new Cliente("10","Alisson","99884811","99995522114","alisson@teste.com");
+$cliente[0] = new Cliente("1","David Luiz de Moura","96031536","01019200910","david@teste.com");
+$cliente[1] = new Cliente("2","Vicente Luiz de Moura","96565456","12548512547","vicente@teste.com");
+$cliente[2] = new Cliente("3","Patrick Deleon Schvartz","92547411","00015215222","patrick@teste.com");
+$cliente[3] = new Cliente("4","Sirlei Isabel Fae de Moura","91221122","11221122122","sirlei@teste.com");
+$cliente[4] = new Cliente("5","Paulo Sato","94784512","52885522112","sirlei@teste.com");
+$cliente[5] = new Cliente("6","Wesley Safadao","91227788","99556622321","wesley@safadao.com");
+$cliente[6] = new Cliente("7","Natacha Cerchiari Mattos","95774411","99552211447","natacha@teste.com");
+$cliente[7] = new Cliente("8","Rogerio Lo Bozza","99992211","11448855221","rogerio@teste.com");
+$cliente[8] = new Cliente("9","Guilherme Osty Washigton","91242424","24242524242","guilherme@teste.com");
+$cliente[9] = new Cliente("10","Alisson Diniz Marcondes","99884811","99995522114","alisson@teste.com");
--- index.php
+++ index.php
@@ -11,15 +11,63 @@ if($_GET['ordem'] == 1)
     rsort($cliente);
 }
 
-foreach($cliente as $cli) {
+?>
 
-    echo "<a href='dados.php?id=".$cli->id."'>".$cli->nome."</a><br />";
 
-}
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-if($_GET['ordem'] == 1)
-{
-    echo "<br /><br /><a href='?ordem=0'>Ordenar Crescente</a><br />";
-} else {
-    echo "<br /><br /><a href='?ordem=1'>Ordenar Decrescente</a><br />";
-}
+<title>PHPOO - School Of Net</title>
+
+    <!-- Bootstrap -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="css/download.css" rel="stylesheet" media="screen">
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+</head>
+
+<body class="fundo">
+    <div class="linha"></div>
+    <div class="container topo">
+		<div class="row">
+         <?   if($_GET['ordem'] == 1)
+            {
+                echo '<form class="form-search span6" id="busca" name="busca" action="?ordem=0" method="post">
+                <div class="input-prepend pesquisar">
+                    <button type="submit" class="btn buscar gradient">Ordem Crescente</button>
+                </div>
+    		</form>';
+            } else {
+             echo '<form class="form-search span6" id="busca" name="busca" action="?ordem=1" method="post">
+                <div class="input-prepend pesquisar">
+                    <button type="submit" class="btn buscar gradient">Ordem Decrescente</button>
+                </div>
+    		</form>';
+         }
+         ?>
+							<div class="titulo span12">LISTA DE CLIENTES
+			</div>
+					</div>
+        <div class="dados">
+
+        <?    foreach($cliente as $cli) {
+
+            echo "<div class='cinza'>
+                <div class='row'>
+                    <div class='span9 nome'>
+                    ".$cli->nome."
+                    </div>
+                    <a href='dados.php?id=".$cli->id ."'><div class='span2 exportar'><i class='icon-download-alt icon-white'></i> ver dados</div></a>
+                </div>
+            </div>";
+            // echo "<a href='dados.php?id=".$cli->id."'>".$cli->nome."</a><br />";
+
+            }
+        ?>
+
+		</div>
+    </div>
+</body>
+</html>
