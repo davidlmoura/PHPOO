ad8198a448030269f3051fe10659ecc96a85599c
David Moura <davidmoura@gmail.com>
2016-03-20 17:03:53 -0300

    Adicionado botões para ordenação do array (ASC e DESC)

--- index.php
+++ index.php
@@ -2,8 +2,24 @@
 
 require_once "Cliente.php";
 
+function cmp($a, $b) {
+    return $a['id'] > $b['id'];
+}
+
+if($_GET['ordem'] == 1)
+{
+    rsort($cliente);
+}
+
 foreach($cliente as $cli) {
 
     echo "<a href='dados.php?id=".$cli->id."'>".$cli->nome."</a><br />";
 
+}
+
+if($_GET['ordem'] == 1)
+{
+    echo "<br /><br /><a href='?ordem=0'>Ordenar Crescente</a><br />";
+} else {
+    echo "<br /><br /><a href='?ordem=1'>Ordenar Decrescente</a><br />";
 }
